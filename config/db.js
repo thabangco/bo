f2795416c8fe7963333eef2bc728fe3d82c64377
thabang@yalu.co.za <thabang@yalu.co.za>
2020-04-10 05:56:43 +0200

    update 2

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 const config = require('config');
-const db = config.get('mongoURI');
+//const db = config.get('mongoURI');
 
 const connectDB = async () => {
 	try {
-		await mongoose.connect(db, {
+		await mongoose.connect("mongodb+srv://thabangcreates:thabang.Creates1@cluster0-3dyf0.mongodb.net/no2?retryWrites=true&w=majority", {
 			useNewUrlParser: true,
 			useCreateIndex: true,
 			useFindAndModify: false,
