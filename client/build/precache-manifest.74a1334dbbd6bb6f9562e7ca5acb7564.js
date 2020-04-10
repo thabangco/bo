44a60826614ed2d80252023b7705072f707a1aef
thabang@yalu.co.za <thabang@yalu.co.za>
2020-04-10 05:40:27 +0200

    initial commit

---
+++ client/build/precache-manifest.74a1334dbbd6bb6f9562e7ca5acb7564.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58d5006307efd9af74cc633056b944de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9b8487407c4cfc43be5",
+    "url": "/static/css/main.83648ea1.chunk.css"
+  },
+  {
+    "revision": "c04d96349dc15a967ac1",
+    "url": "/static/js/2.82715dcb.chunk.js"
+  },
+  {
+    "revision": "d4773cfc8952e56a2df4dd3d885d1f23",
+    "url": "/static/js/2.82715dcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9b8487407c4cfc43be5",
+    "url": "/static/js/main.2748f04f.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
